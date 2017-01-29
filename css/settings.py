4ca68aa4c9d17c5d98cd63ae96b3e2d869bb2fef
vdinovi <vito.dinovi@gmail.com>
2017-01-28 18:06:22 -0800

    rebasing prod

--- css/settings.py
+++ css/settings.py
@@ -76,6 +76,9 @@ WSGI_APPLICATION = 'css.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+    'css-db': {
+        'NAME': css 
+    }
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
