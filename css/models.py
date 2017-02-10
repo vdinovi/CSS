3bfc54f32453d164d9290ec611a353fc30619d73
Paula Ledgerwood <Paula Ledgerwood>
2017-02-10 09:16:52 -0800

    updated room tests

--- css/models.py
+++ css/models.py
@@ -1,123 +1,55 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import Group
-from django.conf import settings
-import MySQLdb
-import re
 
-# ---------- User Models ----------
-class CUserManager(models.Manager):
-    # Verify email is valid
-    def is_valid_email(self, email): 
-        # @TODO fix regex
-        #if re.match(r'\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b', email) is None:
-        #    raise ValueError("Attempted CUser creation"+ 
-        #                     "with invalid email address")
-        return email
+class User(AbstractUser):
+    pass
 
-    # @TODO Come up with password patten and validate it here
-    def is_valid_password(self, password):
-        # @TODO should compare password hash
-        #if password is '':
-        #    raise ValueError("Attempted CUser creation with invalid password")
-        return password
-
-    # Verify user_type is either 'scheduler' or 'faculty'
-    def is_valid_user_type(self, user_type):
-        if user_type != 'scheduler' and user_type != 'faculty':
-            raise ValueError("Attempted CUser creation with invalid user_type")
-        return user_type
-
-    def create_cuser(self, email, password, user_type):
-        try:
-            user = self.create(user=User.objects.create_user(
-                                   username=self.is_valid_email(email), 
-                                   email=self.is_valid_email(email),
-                                   password=self.is_valid_password(password)),
-                               user_type=self.is_valid_user_type(user_type))
-        except MySQLdb.IntegrityError as e:
-            raise
-        return user
-
-    def get_faculty(self):
-        return self.filter(user_type='faculty')
-
-    def get_scheduler(self):
-        return self.filter(user_type='faculty')
-
-class CUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    user_type = models.CharField(max_length=16)
-
-    objects = CUserManager()
- 
-class FacultyDetails(models.Model):
-    # The user_id uses the User ID as a primary key.
-    # Whenever this User is deleted, this entry in the table will also be deleted
-    user = models.OneToOneField(CUser, on_delete=models.CASCADE, blank=False)
-    target_workload = models.IntegerField() # in hours
-    changed_preferences = models.CharField(max_length=1) # 'y' or 'n' 
-
-# ---------- Resource Models ----------
-# Room represents department rooms
 class Room(models.Model):
-   name = models.CharField(max_length=32)
-   description = models.CharField(max_length=256, null=True)
-   capacity = models.IntegerField(default=0)
-   notes = models.CharField(max_length=1024, null=True)
-   equipment = models.CharField(max_length=1024, null=True)
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=256, null=True)
+    capacity = models.IntegerField(default=0)
+    notes = models.CharField(max_length=1024, null=True)
+    equipment = models.CharField(max_length=1024, null=True)
 
-# Course represents a department course offering
+    def get_name(): 
+        return self.name
+
+    def get_description():
+        return self.description
+        
+    def get_capacity():
+        return self.capacity
+
+    def get_notes():
+        return self.notes
+
+    def get_equipment():
+        return self.equipment 
+
 class Course(models.Model):
     course_name = models.CharField(max_length=16)
     equipment_req = models.CharField(max_length=2048, null=True)
     description = models.CharField(max_length=2048, null=True)
 
-    def get_name(self):
-    	return self.course_name
+class FacultyDetails(models.Model):
+    # The user_id uses the User ID as a primary key.
+    # Whenever this User is deleted, this entry in the table will also be deleted
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    target_workload = models.IntegerField() # in hours
+    changed_preferences = models.CharField(max_length=1) # 'y' or 'n' 
 
-    def get_equipment_req(self):
-    	return self.equipment_req
-
-    def get_description(self):
-    	return self.description
-
-# SectionType contains all the defined section types the department allows
-class SectionType(models.Model):
-    section_type = models.CharField(max_length=32, primary_key=True) # eg. lecture or lab
-
-
-# WorkInfo contains the user defined information for specific Course-SectionType pairs
-# Each pair has an associated work units and work hours defined by the department
-class WorkInfo(models.Model): 
-    class Meta:
-        unique_together = (("course", "section_type"),)
-    course = models.ForeignKey(Course, on_delete = models.CASCADE)
-    section_type = models.ForeignKey(SectionType, on_delete = models.CASCADE)
-    work_units = models.IntegerField(default = 0)
-    work_hours = models.IntegerField(default = 0)
-
-# ---------- Scheduling Models ----------
-# Schedule is a container for scheduled sections and correponds to exactly 1 academic term
 class Schedule(models.Model):
     academic_term = models.CharField(max_length=16, unique=True) # eg. "Fall 2016"
-    state = models.CharField(max_length=16, default="active") # eg. active or finalized 
+    state = models.CharField(max_length=16) # eg. active or finalized 
 
-    def finalize_schedule(self):
-    	self.state = "finalized"
-
-    def return_to_active(self):
-    	self.state = "active"
-
-# Section is our systems primary scheduled object
-# Each section represents a department section that is planned for a particular schedule
 class Section(models.Model):
-    schedule = models.OneToOneField(Schedule, on_delete=models.CASCADE, unique=True)
-    course = models.OneToOneField(Course, on_delete=models.CASCADE, unique=True)
+    schedule_id = models.OneToOneField(Schedule, on_delete=models.CASCADE, unique=True)
+    course_id = models.OneToOneField(Course, on_delete=models.CASCADE, unique=True)
     start_time = models.TimeField()
     end_time = models.TimeField()
     days = models.CharField(max_length = 8)    # MWF or TR
-    faculty = models.ForeignKey(CUser, null = True, on_delete = models.SET_NULL, default = models.SET_NULL)
+    faculty = models.OneToOneField(User, null = True, on_delete = models.SET_NULL, default = models.SET_NULL)
     room = models.OneToOneField(Room, null = True, on_delete = models.SET_NULL, default = models.SET_NULL)
     section_capacity = models.IntegerField(default = 0)
     students_enrolled = models.IntegerField(default = 0)
@@ -127,3 +59,13 @@ class Section(models.Model):
     fault = models.CharField(max_length = 1) # y or n
     fault_reason = models.CharField(max_length = 8) # faculty or room
 
+class SectionType(models.Model):
+    section_type = models.CharField(max_length=32, primary_key=True) # eg. lecture or lab
+
+class FacultyWorkInfo(models.Model):
+    class Meta:
+        unique_together = (("course_id", "section_type"),)
+    course_id = models.ForeignKey(Course, on_delete = models.CASCADE)
+    section_type = models.ForeignKey(SectionType, on_delete = models.CASCADE)
+    work_units = models.IntegerField(default = 0)
+    work_hours = models.IntegerField(default = 0)
--- tests/test_room.py
+++ tests/test_room.py
@@ -3,18 +3,90 @@ from css.models import *
 
 class RoomTestCase(TestCase): 
     def setUp(self):
+        # room with all attributes defined
         Room.objects.create(name="14-255",
                             description="Graphics lab",
                             capacity=35,
                             notes="blah", 
                             equipment="whiteboard, computers, projector")
+        # room without equipment defined
         Room.objects.create(name="180-101",
                             description="Chemistry lab",
                             capacity=25,
-                            notes="bleh", 
-                            equipment="whiteboard, microscopes, sinks")
+                            notes="bleh")
+        # room with out notes defined
+        Room.objects.create(name="4-111",
+                            description="Engineering IV",
+                            capacity=20,
+                            equipment="whiteboard, large tables")
 
-    def test_room_names(self): 
-        # test that 
+    def test_room_get_name(self): 
+        """ Test that names are retrieved properly """
         graphics = Room.objects.get(name="14-255")
+        chem = Room.objects.get(name="180-101")
+        eng = Room.objects.get(name="4-111")
+        self.assertEqual(graphics.get_name(), "14-255")
+        self.assertEqual(chem.get_name(), "180-101")
+        self.assertEqual(eng.get_name(), "4-111")
+
+    def test_room_set_name(self): 
+        """ Test that names are set properly """
+        graphics = Room.objects.get(name="14-255")
+        chem = Room.objects.get(name="180-101")
+        eng = Room.objects.get(name="4-111")
+        graphics.name = "14-256"
+        chem.name = "180-102"
+        eng.name = "4-112"
+        graphics.save()
+        chem.save()
+        eng.save()
+        self.assertEqual(graphics.get_name(), "14-256")
+        self.assertEqual(chem.get_name(), "180-102")
+        self.assertEqual(eng.get_name(), "4-112")
+
+    def test_room_get_capacity(self):
+        """ Test that capacity is retrieved properly """
+        graphics = Room.objects.get(name="14-255")
+        chem = Room.objects.get(name="180-101")
+        eng = Room.objects.get(name="4-111")
+        self.assertEqual(graphics.get_capacity(), 35)
+        self.assertEqual(chem.get_capacity(), 25)
+        self.assertEqual(eng.get_capacity(), 20)
+
+    def test_room_set_capacity(self):
+        """ Test that capacity is retrieved properly """
+        graphics = Room.objects.get(name="14-255")
+        chem = Room.objects.get(name="180-101")
+        eng = Room.objects.get(name="4-111")
+        self.assertEqual(graphics.get_capacity(), 35)
+        self.assertEqual(chem.get_capacity(), 25)
+        self.assertEqual(eng.get_capacity(), 20)
+
+    def test_room_get_description(self):
+        """ Test that description is retrieved properly """
+        graphics = Room.objects.get(name="14-255")
+        chem = Room.objects.get(name="180-101")
+        eng = Room.objects.get(name="4-111")
+        self.assertEqual(graphics.get_description(), "Graphics lab")
+        self.assertEqual(chem.get_description(), "Chemistry lab")
+        self.assertEqual(eng.get_description(), "Engineering IV")
+
+    def test_room_get_notes(self):
+        """ Test that notes are retrieved properly """
+        graphics = Room.objects.get(name="14-255")
+        chem = Room.objects.get(name="180-101")
+        eng = Room.objects.get(name="4-111")
+        self.assertEqual(graphics.get_notes(), "blah")
+        self.assertEqual(chem.get_notes(), "bleh")
+        self.assertEqual(eng.get_notes(), None)
+
+    def test_room_get_equipment(self):
+        """ Test that equipment retrieved properly """
+        graphics = Room.objects.get(name="14-255")
+        chem = Room.objects.get(name="180-101")
+        eng = Room.objects.get(name="4-111")
+        self.assertEqual(graphics.get_equipment(), "whiteboard, computers, projector")
+        self.assertEqual(chem.get_equipment(), None)
+        self.assertEqual(eng.get_equipment(), "whiteboard, large tables")
+
 
