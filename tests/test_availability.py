3f1ffb5690bcbfa6cb59c9d92c05d0f9dcf2b6cc
Paula Ledgerwood <Paula Ledgerwood>
2017-02-12 20:26:50 -0800

    updated availability again

--- tests/test_availability.py
+++ tests/test_availability.py
@@ -7,18 +7,28 @@ class AvailabilityTestCase(TestCase):
                        user_type='faculty'):
         return CUser.objects.create_cuser(email, password, user_type)
 
+    def setUp(self):
+        create_faculty()
+
+    def tearDown(self):
+        CUser.objects.get(email="email@email.com").delete()
+
     def test_availability_invalid_faculty_id(self):
-        faculty = create_faculty()
-        self.assertRaises(ValidationError, Availability.create("testunique@email.com", "BLAH", None, None, None))
+        self.assertRaises(ValidationError, Availability.create("email_unique@email.com", None, None, None, None))
 
     def test_availability_invalid_days_of_week(self):
-        self.assertRaises(ValidationError, Availability.create())
+        self.assertRaises(ValidationError, Availability.create("email@email.com", "HELLO", None, None, None))
 
     def test_availability_invalid_start_time(self):
-        self.assertRaises(ValidationError, Availability.create())
+        # TODO: add actual test for invalid start time
+        self.assertRaises(ValidationError, Availability.create("email@email.com", "MWF", None, None, None))
 
     def test_availability_invalid_end_time(self):
-        self.assertRaises(ValidationError, Availability.create())
+        # TODO: add actual test for invalid end time
+        self.assertRaises(ValidationError, Availability.create("email@email.com", "TR", None, None, None))
 
     def test_availability_invalid_level(self):
-        self.assertRaises(ValidationError, Availability.create())
+        # TODO: add actual test for invalid level
+        self.assertRaises(ValidationError, Availability.create("email@email.com", "MWF", , None, None))
+
+
